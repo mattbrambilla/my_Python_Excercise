27a8598e37da47d67fd04bd2c78738312bfea3ae
Mattia Brambilla <m.brambilla@live.it>
2023-06-01 17:03:24 +0200

    Write my first python program

---
+++ primi passi/primo esercizio.py
@@ -0,0 +1,15 @@
+#This is my first Python Program, i'm excited :)
+
+robot_name = "Chappie"
+robot_age = 1
+
+print("Ciao, il mio nome é" + " " + robot_name + " " + "e ho " + str(robot_age) + " anno")
+
+user_name = input("Tu come ti chiami?")
+print("Ciao " + user_name)
+
+user_age = int(input("Quanti anni hai? "))
+age_difference = user_age - robot_age #differenza età
+
+print("Hai" + (str(user_age)) + " " + "anni? " + "Sono " + str(age_difference) + " anni più di me!"
+print = "A presto!"
